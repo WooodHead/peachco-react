78e03d2dbd000e114b0e341c4e466adf9eede952
Oscar Villalta <bagglerock@gmail.com>
2018-06-19 15:51:13 -0400

    ebay getcat route works

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
-//const ebayRoutes = require("./ebay");
+const ebayRoutes = require("./ebay/ebay");
 const db = require("./db/db");
 //const barcode = require("./barcode");
 
 router.use("/db", db);
-//router.use("/ebay", ebayRoutes);
+router.use("/ebay", ebayRoutes);
 //router.use("/barcode", barcode);
 
 module.exports = router;
