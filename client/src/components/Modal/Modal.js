568e3eb6aefa0455c2064e3e8ec4f2aa863cd60c
Oscar Villalta <bagglerock@gmail.com>
2018-07-04 00:43:19 -0400

    lots of styling in the modal

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from "../Button";
 import LinkButton from '../Button/LinkButton';
 import "./Modal.css";
 
@@ -10,34 +9,58 @@ export const Modal = props => {
 
         <div className="modal">
             <div className="modal-content animate">
+                <div className="modal-close-container"><i class="fas fa-times fa-3x" onClick={props.closeModal}></i></div>
                 <div className="item-info-container">
-                    <ul>
-                        <li>{props.item.sku}</li>
-                        <li>{props.item.brand}</li>
-                        <li>{props.item.collection}</li>
-                        <li>{props.item.type}</li>
-                        <li>{props.item.color}</li>
-                        <li>{props.item.retail}</li>
-                    </ul>
+
+                    <div className="modal-heading">
+                        <h4>{props.item.brand} - {props.item.collection} {props.item.color} {props.item.type}</h4>
+                    </div>
+                    <div className="modal-info">
+                        
+                        <p>SKU: {props.item.sku}</p>
+                        <p>CustomID: {props.item.secPic}</p>
+                        <p><br/></p>
+                        <p>{props.item.f_1}</p>
+                        <p>{props.item.f_2}</p>
+                        <p>{props.item.f_3}</p>
+                        <p>{props.item.f_4}</p>
+                        <p>{props.item.f_5}</p>
+                        <p><br/></p>
+                        <p>Retail: {props.item.retail}</p>
+                    </div>
+                    <div className="modal-images">
+                        <div>
+                            <img src={(props.item.pic !== "null" && props.item.pic !== "") ? 
+                                (`http://www.thepeachco.com/ebay/images/sticky/${props.item.pic}/1.jpg`) : 
+                                ("./No_image_available.svg")} width="300"/>
+                        </div>
+                        <div>
+                            <img src={(props.item.secPic !== "null" && props.item.secPic !== "") ? 
+                                (`http://www.thepeachco.com/ebay/images/${props.item.secPic}/1.jpg`) : 
+                                ("./No_image_available.svg")} width="300"/>
+                        </div>
+                    </div>
+
                 </div>
-                <div className="close-container">
-                    <LinkButton
+                <div className="modal-button-container">
+                    <div>
+                        <LinkButton
                         to={"/build/"} 
                         type={"similar"}
                         id={props.item.id}
-                    >
-                    Similar
-                    </LinkButton>
-                    <LinkButton
+                        >
+                        Similar
+                        </LinkButton>
+                    </div>
+                    <div>
+                        <LinkButton
                         to={"/build/"} 
                         type={"exact"}
                         id={props.item.id}
-                    >
-                    Exact
-                    </LinkButton>
-                </div>
-                <div>
-                    <Button func={props.closeModal} name="Close"/>
+                        >
+                        Exact
+                        </LinkButton>
+                    </div>
                 </div>
             </div>
         </div>
