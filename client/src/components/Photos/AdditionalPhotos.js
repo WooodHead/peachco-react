c7bec2af88a4ee1856c775880a8d0c04a0159b0d
Oscar Villalta <bagglerock@gmail.com>
2018-07-05 09:12:04 -0400

    some styling changes to the additional photos component

--- client/src/components/Photos/AdditionalPhotos.js
+++ client/src/components/Photos/AdditionalPhotos.js
@@ -1,8 +1,14 @@
 import React from "react";
+import urlExists from "url-exists";
 import "./AdditionalPhotos.css";
 
 export const AdditionalPhotos = props => {
-  const images = ["", "", "", "", "", "", "", "", "", "", "", ""];
+
+    const images = [props.secPic,'','','','','','','','',''];
+
+    const addImage = () => {
+        console.log("blah")
+    }
 
   return (
       <div className="section-wrapper">
@@ -12,11 +18,12 @@ export const AdditionalPhotos = props => {
                 <div 
                     className="photo-wrapper"
                     key={i}
-                    onClick={() => console.log(i)}
+                    onClick={() => addImage()}
                 >
-                    {(image !== "" && image !== undefined) ? 
-                     (`<img src="http://www.thepeachco.com/ebay/images/${image}/${i+1}.jpg" alt="something"/>`) : 
-                     (<i className="fas fa-plus" />)}
+                    {image !== "" ?
+                     <img className="additional-image" src={`http://www.thepeachco.com/ebay/images/${image}/${i+1}.jpg`} alt="extra"/> : 
+                     <i className="fas fa-plus"/>
+                    }
                 </div>
             ))}
         </div>
