3d17064a5966d83e01cf2f4777d81d1bd2c6e044
Oscar Villalta <bagglerock@gmail.com>
2018-07-16 18:58:32 -0400

    heroku install

--- models/index.js
+++ models/index.js
@@ -8,12 +8,12 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
+console.log(config);
+
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  console.log("we are in production");
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-  console.log("not in production");
 }
 
 fs
