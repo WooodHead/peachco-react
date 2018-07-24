ff0650fccad34c4b570cca279b58a325c6545a86
Oscar Villalta <bagglerock@gmail.com>
2018-07-24 03:51:44 -0400

    added link back to search

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,7 +4,8 @@ import "./Header.css";
 const Header = () => {
     return (
         <header className="header">
-            <h4 className="header-title">PeachCo Lister 3.0</h4>
+            <a href="../search"><h4 className="header-title">PeachCo Lister 3.0</h4></a>
+            
         </header>
     )
 }
