8c89d84c0a1262e4305f0d7216e8088c3cc4727a
Oscar Villalta <bagglerock@gmail.com>
2018-08-15 03:20:29 -0400

    added small tester component to test camera for barcode scanner

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Build from "./pages/Build";
 import Header from "./components/Header/Header";
 import Login from "./pages/Login";
 import NoMatch from "./pages/NoMatch";
+import Random from "./pages/Random";
+import React from "react";
 import Search from "./pages/Search/Search";
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
 import "./App.css";
 
 const App = () => {
@@ -16,6 +18,7 @@ const App = () => {
           <Route exact path="/" component={Login}/>
           <Route exact path="/search/" component={Search}/> 
           <Route exact path="/build/" component={Build}/>
+          <Route exaxt path="/random/" component={Random}/>
           <Route component={NoMatch}/>
         </Switch>
       </main>
